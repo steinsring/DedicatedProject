df463d704fe18e9d27cbb3fe268cd108f30639a7
ahnw00 <ahnw00@naver.com>
2025-04-12 01:28:47 +0900

    animinstance and retargetting

---
+++ Source/DedicatedProject/Public/PE_AnimInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PE_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDPROJECT_API UPE_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
